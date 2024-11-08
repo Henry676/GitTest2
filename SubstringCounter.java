f3d8ff33c11d9f7f692628d112b1a7d6d974761f
Henry676 <hdhm2001@gmail.com>
2024-11-08 15:29:25 -0600

    Substrings Algorithm

---
+++ SubstringCounter.java
@@ -0,0 +1,38 @@
+package GitTest2;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubstringCounter{
+    private String stringI;
+
+    public SubstringCounter(String args){
+        this.stringI = args;
+    }
+
+    public List<String> createSubstrings(){
+        List<String> substrings = new ArrayList<>();
+
+        for(int start = 0; start < stringI.length(); start++){
+            int countZeros = 0, countOnes = 0;
+            for(int end = start; end < stringI.length(); end++){
+                if(stringI.charAt(end) == '0') countZeros++;
+                else countOnes++;
+                if(countOnes == countZeros)
+                substrings.add(stringI.substring(start, end+1));
+            }
+        }
+        return substrings;
+    }
+
+    public void showSubstrings(List<String> result){
+        for(int i = 0; i<result.size(); i++){
+            System.out.println("Substring #"+(i+1)+": "+result.get(i));
+        }
+    }
+    public static void main(String[] args) {
+        SubstringCounter s = new SubstringCounter(args[0]);
+        List<String> result = s.createSubstrings();
+        System.out.println("Substrings total: "+result.size());
+        s.showSubstrings(result);
+    }
+}
